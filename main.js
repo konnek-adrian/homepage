cd9540c751ddfaeaa68d21229e76ebd80adaa885
Adrian Konnek <konnek.adrian@gmail.com>
2021-04-29 21:17:55 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const firstname = 'Ardianek';
+const age = 26;
+console.log('No i czego tu kurwa szukasz');
+console.log(firstname);
+console.log(age);
+console.log(`Siema, nazywam się ${firstname} i mam ${age} lat.`)
