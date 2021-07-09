d68438f9ec9a5db020e393b51abb785aa9cc50c1
Adrian Konnek <konnek.adrian@gmail.com>
2021-07-09 12:18:47 +0200

    background gradient and flexbox on the nav bar

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
-const firstname = 'Ardianek';
+const firstname = "Ardianek";
 const age = 26;
-console.log('No i czego tu kurwa szukasz');
+console.log("Noo heeeeeeej");
 console.log(firstname);
 console.log(age);
-console.log(`Siema, nazywam się ${firstname} i mam ${age} lat.`)
+console.log(`Siema, nazywam się ${firstname} i mam ${age} lat.`);
+
+const heading = document.querySelector(".main__heading--js");
+heading.innerHTML = `Siema, nazywam się ${firstname} i mam ${age} lat.`;
+const emptyParagraph = document.querySelector(".week-summary__description--js");
+emptyParagraph.innerHTML = `Nawet uzupełniłem treść javascriptem!`;
+
+function greet(age, firstname) {
+  console.log(`Siema, nazywam się ${firstname} i mam ${age} lat.`);
+}
+
+greet(age, "Adrian");
+
+function createContent(querySelectorContent, content) {
+  let element = document.querySelector(querySelectorContent);
+  element.innerHTML += content;
+}
+createContent('.week-summary__description--js', 'Witaj świecie!');
+
+createContent('.about__heading--js', 'Podmianka JS!');
+
+
+
+const deathStar = {
+  diameter: 120000,
+fire: (target) => {
+  console.log(`${target} destroyed`)
+},
+isOperating: true,
+levels: 357,
+name: 'Death Star',
+population: 10000,
+}
+
+console.log(deathStar)
